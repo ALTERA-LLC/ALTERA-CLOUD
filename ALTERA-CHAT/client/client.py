51083a8a889ff116fe314e1feffbd077a3522f5f
ALTERA-LLC <62931899+ALTERA-LLC@users.noreply.github.com>
2020-11-05 18:02:11 +0800

    added stuff

--- ALTERA-CHAT/client/client.py
+++ ALTERA-CHAT/client/client.py
@@ -15,6 +15,7 @@ class Main:
         self.root.resizable(0, 0)
         self.root.config(bg='black')
         self.running = True
+        self.messages = ['', '', '', '', '', '', '', '', '', '']
         self.listbox = Listbox(self.root, font=('Consolas', 15, 'bold'), bg='black', fg='white', width=52)
         threading.Thread(target=lambda: asyncio.run(self.join())).start()
         self.root.mainloop()
@@ -38,7 +39,7 @@ class Main:
                   font=('Consolas', 15, 'bold'), bg='black', fg='white').pack()
             return
 
-        def send_un(event):
+        def send_un():
             self.sock.send(username.get().encode('utf-8'))
             username.destroy()
             self.root.unbind('<Return>')
@@ -46,7 +47,7 @@ class Main:
 
         username = Entry(self.root, font=('Consolas', 15, 'bold'), bg='black', fg='white', insertbackground='white')
         username.pack(side=TOP)
-        self.root.bind('<Return>', func=send_un)
+        self.root.bind('<Return>', func=lambda event: send_un())
 
     async def connect(self):
         try:
@@ -70,30 +71,45 @@ class Main:
                           insertbackground='white', bd=3)
         self.msg_input.place(x=0, y=369)
         self.root.bind('<Return>', func=self.sendmsg)
-        recv_thread = threading.Thread(target=self.recv)
+        self.root.bind("<Button-3>", func=lambda event: self.listbox.selection_clear(0, END))
+        recv_thread = threading.Thread(target=lambda: asyncio.run(self.recv()))
         recv_thread.setDaemon(True)
         recv_thread.start()
 
 
     def sendmsg(self, event):
-        msg = str(self.msg_input.get("1.0", END))
+        msg = str(self.msg_input.get("1.0", END)).rstrip()
         if msg == '':
             pass
         else:
             self.sock.send(msg.encode('utf-8'))
             self.msg_input.delete('1.0', END)
 
-    def recv(self):
+    async def change_msg(self, pos, msg):
+        self.listbox.delete(pos)
+        self.listbox.insert(pos, msg)
+
+    async def recv(self):
+        for item in self.messages:
+            self.listbox.insert(0, item)
         while self.running:
             try:
-                hello = self.sock.recv(1024).decode('utf-8')
-                self.listbox.insert(0, hello)
+                msg = self.sock.recv(1024).decode('utf-8')
+                self.messages.remove(self.messages[0])
+                self.messages.append(msg)
+                for num in range(1, 9):
+                    await self.change_msg(num, self.messages[num])
+                self.listbox.insert(9, msg)
+
+
+
+
             except:
                 # destroy widgets
-                self.chat_verlabel.destroy()
-                self.listbox.destroy()
-                self.message_label.destroy()
-                self.msg_input.destroy()
+                self.chat_verlabel.place(x=1000, y=1000)
+                self.listbox.place(x=1000, y=1000)
+                self.message_label.place(x=1000, y=1000)
+                self.msg_input.place(x=1000, y=1000)
                 Label(self.root, text='Server connection lost\nplease try again later',
                       font=('Consolas', 15, 'bold'), bg='black', fg='white').pack()
                 self.running = False
